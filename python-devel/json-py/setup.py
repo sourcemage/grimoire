a97780777f2d59b15f8893847b75cedb066a7f4a
Jeremy Blosser <jblosser@fawkes.sourcemage.org>
2006-04-24 11:36:25 -0500

    sync ./python-devel/json-py from stable-rc 0.4

---
+++ python-devel/json-py/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name="json-py",
+    version="3.4",
+    author="Patrick D. Logan",
+    url="http://sourceforge.net/projects/json-py",
+    license="LGPL",
+    py_modules=['json', 'jsontest', 'minjson'],
+)
