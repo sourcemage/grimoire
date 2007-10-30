bae37e26c29840b7fcb7e96527a2b3263e29ef10
Treeve Jelbert <treeve@sourcemage.org>
2007-10-30 10:09:03 +0100

    kdebase-workspace4  - update desktop settings

---
+++ kde4/kdebase-workspace4/kde4.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+# setup KDE4 environment for SMGL
+source /etc/profile.d/qt.sh
+
+export PATH=$QTDIR/bin/qt4:$PATH
+export KDEDIR=/opt
+export KDEDIRS=$KDEDIR
+export KDE_DATA_DIRS=$KDEDIR/share
+export PATH=$KDEDIR/bin:$PATH
+export XDG_CONFIG_DIRS=$KDEDIR/etc/xdg
+export QT_PLUGIN_PATH=$KDEDIR/lib/plugins
+export LD_LIBRARY_PATH=$KDEDIR/lib:$LD_LIBRARY_PATH
+export PKG_CONFIG_PATH=$KDEDIR/lib/pkgconfig:$PKG_CONFIG_PATH
+#export KDE_COLOR_DEBUG=1
+
+# this will keep the kde3/4 desktops separate
+export KDEHOME=$HOME/.kde4
+
+# temporary runtime directories
+export KDETMP=${TMPDIR-/tmp}/kde4-$USER
+export KDEVARTMP=/var/tmp/kde4cache-$USER
+
+# Ensure that they exist
+mkdir -p $KDEDIR $KDETMP $KDEVARTMP
+
+
+
+
