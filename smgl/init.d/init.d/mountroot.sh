d29b6c695bca8d97ead4ecbb187635faa3e51d3a
Jaka Kranjc <lynxlynxlynx@sourcemage.org>
2008-08-22 15:31:21 +0200

    mountroot.sh: don't unconditionally overwrite the mtab and do it       properly #14665

--- smgl/init.d/init.d/mountroot.sh
+++ smgl/init.d/init.d/mountroot.sh
@@ -78,7 +78,9 @@ start()
   echo "Mounting root file system read/write..."
   {
     mount    -n -o remount,rw / &&
-    echo     > /etc/mtab        &&
+    if ! [[ -h /etc/mtab ]]; then
+      builtin echo > /etc/mtab
+    fi &&
     mount    -f -o remount,rw /
   } || exit 1
 
