bd37a1f13d3b8d262dc1b1f7578ed5f3ee50d933
Treeve Jelbert <treeve@sourcemage.org>
2015-04-29 15:40:31 +0200

    add all plasma-5.3.0 spells

---
+++ plasma/plasma-workspace/plasma.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+if [ "$USER" != "root" ];then
+
+# setup KDE5 environment for SMGL
+export QTDIR=/opt/qt5
+export KF5=$QTDIR
+export PATH+=":$KF5/bin"
+export XDG_CONFIG_DIRS=/etc/xdg
+#export XDG_DATA_DIRS+=:$QTDIR/share:/usr/share
+export QT_PLUGIN_PATH+=:$KF5/lib/plugins
+#export QT_PLUGIN_PATH=$KF5/lib/plugins:$QTDIR/plugins:
+
+export QML2_IMPORT_PATH=$KF5/lib/qml
+export QML_IMPORT_PATH=$QML2_IMPORT_PATH
+export XCURSOR_PATH=$KF5/share/icons:~/.icons:/usr/share/icons:/usr/share/pixmaps
+
+# allow running kde4 apps in a plasma5 session
+#  put qt4 first, otherwise kamil/akonadi cannot find plugins
+  export KDE4=/opt/qt4
+  if [[ -f $KDE4/bin/kdeinit4  ]];then
+    export XDG_DATA_DIRS+=":$KDE4/share"
+    export PATH+=":$KDE4/bin"
+    export QT_PLUGIN_PATH+=":$KDE4/lib/plugins"
+  fi
+# temporary runtime directories
+export XDG_RUNTIME_DIR=${TMPDIR-/tmp}/plasma-$USER
+
+# Ensure that they exist
+mkdir -p $XDG_RUNTIME_DIR
+
+fi
+
+# if already in kde4, need a separate dbus session for kf5
+# eval `dbus-launch`
+# before using kf5, need to run:
+#    kbuildsycoca5
+# if kwin is already running, use the following command
+# kwin_x11 --replace &
+
