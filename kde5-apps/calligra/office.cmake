c725a77dd7bc9d1fa7656a9a4ad3c959ae5c6845
Treeve Jelbert <treeve@sourcemage.org>
2016-12-05 16:36:17 +0100

    calligra3

---
+++ kde5-apps/calligra/office.cmake
@@ -0,0 +1,24 @@
+#defines the set of products commonly wanted for classic Desktop environment
+
+calligra_define_productset(OFFICE "Calligra for Office"
+    OPTIONAL
+        # apps
+#        AUTHOR
+#        BRAINDUMP
+#        FLOW
+#        KARBON
+#        PLAN
+        SHEETS
+        STAGE
+        WORDS
+        # features
+#        FEATURE_SCRIPTING
+#        FEATURE_RDF
+        # extras
+        APP_CALLIGRA
+        APP_CONVERTER
+        FILEMANAGER
+        OKULAR
+        # docs
+#        DOC
+)
