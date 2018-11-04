bbe1a71937a7f65066536e724e69d66bc67c35e2
Treeve Jelbert <treeve@sourcemage.org>
2018-11-04 22:20:03 +0100

    sddm - fix qt dirs

--- qt5-cmake/sddm/sddm.sh
+++ qt5-cmake/sddm/sddm.sh
@@ -5,9 +5,9 @@ if [ "$USER" != "root" ];then
 QTDIR=/opt/qt5
 export XDG_CONFIG_DIRS=/etc/xdg
 export XDG_DATA_DIRS=$QTDIR/share:/usr/share
-export QT_PLUGIN_PATH=$QTDIR/lib/plugins:$QTDIR/plugins:$QT_PLUGIN_PATH
+export QT_PLUGIN_PATH=$QTDIR/plugins:$QT_PLUGIN_PATH
 
-export QML2_IMPORT_PATH=$QTDIR/lib/qml
+export QML2_IMPORT_PATH=$QTDIR/qml
 export QML_IMPORT_PATH=$QML2_IMPORT_PATH
 export XCURSOR_PATH=$QTDIR/share/icons:~/.icons:/usr/share/icons:/usr/share/pixmaps
 
