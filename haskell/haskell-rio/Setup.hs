69f0a093a54da98dbab0b8977f537e53c9654017
Florian Franzmann <siflfran@hawo.stw.uni-erlangen.de>
2018-11-04 13:31:15 +0100

    haskell/haskell-rio: new spell, a standard library for haskell

---
+++ haskell/haskell-rio/Setup.hs
@@ -0,0 +1,22 @@
+#! /usr/bin/env runhaskell
+
+{- Copyright © 2007 Magnus Therning
+ -
+ - This file is part of dataenc.
+ -
+ - Dataenc is free software: you can redistribute it and/or modify it under
+ - the terms of the GNU Lesser General Public License as published by the
+ - Free Software Foundation, either version 3 of the License, or (at your
+ - option) any later version.
+ -
+ - Dataenc is distributed in the hope that it will be useful, but WITHOUT
+ - ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ - FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+ - License for more details.
+ -
+ - You should have received a copy of the GNU Lesser General Public License
+ - along with dataenc.  If not, see <http://www.gnu.org/licenses/>
+ -}
+
+import Distribution.Simple
+main = defaultMain
